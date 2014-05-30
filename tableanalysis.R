208b9cf051a28295f5cf272496af808dbb171f97
Derek Franks <derekfranks@gmail.com>
2014-05-30 10:33:17 -0500

    Updated "significant difference" matrix and changed the order of when the prop.tests are run and displayed.

--- tableanalysis.R
+++ tableanalysis.R
@@ -8,32 +8,51 @@ table.analysis <- function(var, segment, alpha=.05, detail=FALSE) {
         print(tab_1_chisq)
 #if pvalue of chisq is < alpha, then proceed with pairwise prop comparisons
         if (tab_1_chisq$p.value < alpha) {
-#create a matrix with the combinations based on number of columns
-                comb <- combn(ncol(tab_1),2)  
-                #print("Pair comparisons:")
-                #print(comb)
-                comp_tab <- matrix(nrow=nrow(tab_1),ncol=ncol(tab_1))
-                comp_tab[is.na(comp_tab)]   <- "" 
                 
-#loop through each row of the table and conduct a prop test for each unique column pairing
+                perm <- permutations(ncol(tab_1),2)  
+
+                #create a matrix with the combinations based on number of columns
+                comp_tab <- matrix(nrow=nrow(tab_1),ncol=ncol(tab_1))
+                comp_tab[is.na(comp_tab)] <- "" 
+                
+                #loop through each row of the table and conduct a prop test for EACH column pairing
                 for (i in 1:nrow(tab_1)) {
-                        for (k in 1:ncol(comb)) {
-                                counts <- c(tab_1[i,comb[1,k]], tab_1[i,comb[2,k]])
-                                totals <- c(sum(tab_1[,comb[1,k]]), sum(tab_1[,comb[2,k]]))
+                        for (k in 1:nrow(perm)) {
+                                counts <- c(tab_1[i,perm[k,1]], tab_1[i,perm[k,2]])
+                                totals <- c(sum(tab_1[,perm[k,1]]), sum(tab_1[,perm[k,2]]))
                                 result <- prop.test(counts, totals, correct=F)
-                                result$p.value <- p.adjust(result$p.value, method="bonferroni", n=ncol(comb))
-                                
+                                result$p.value <- p.adjust(result$p.value, method="bonferroni", n=ncol(perm))
                                 #build comp_table that summarizes the results of the pairwise prop tests
                                 if (result$p.value > alpha) {
-                                        comp_tab[i,comb[2,k]] <- paste(comp_tab[i,comb[2,k]], letters[comb[1,k]], sep="")
-                                }
-                                                               if (detail==TRUE) {
-                                        print(paste("Row:",as.character(i)," ", "Pair-comparison:", as.character(k),sep=" "))
-                                        print(result)
+                                        comp_tab[i,perm[k,2]] <- paste(comp_tab[i,perm[k,2]], letters[perm[k,1]], sep="")
                                 }
                         }
                 }
-        print(comp_tab)
+                print(comp_tab)
+                
+                
+
+                comb <- combinations(ncol(tab_1),2)  
+                
+                if (detail==TRUE) {
+                        print("Pair comparisons:")
+                        print(comb)
+                }
+#loop through each row of the table and conduct a prop test for EACH UNIQUE column pairing
+                for (i in 1:nrow(tab_1)) {
+                        for (k in 1:nrow(comb)) {
+                                counts <- c(tab_1[i,comb[k,1]], tab_1[i,comb[k,2]])
+                                totals <- c(sum(tab_1[,comb[k,1]]), sum(tab_1[,comb[k,2]]))
+                                result <- prop.test(counts, totals, correct=F)
+                                result$p.value <- p.adjust(result$p.value, method="bonferroni", n=ncol(comb))
+                                
+                                if (detail==TRUE) {
+                                        print(paste("Row:",as.character(i)," ", "Pair-comparison:", as.character(k),sep=" "))
+                                               print(result)
+                                }
+                        }
+                
+                }
         }
         else {stop("Variables are independent of each other")
         }
